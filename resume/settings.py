a5a8628ef50d7f58058c7afe1c67dd47e78ce823
Benson <bensongathu23@gmail.com>
2022-07-17 16:14:00 +0300

    process

--- resume/settings.py
+++ resume/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 # ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 # Application definition
 
 INSTALLED_APPS = [
